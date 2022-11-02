09f91eaf7f33142a0c72e8f59c56abb0d22fa2b5
AltDaze <dazemoda@gmail.com>
2022-11-02 14:33:30 +0300

    Create feeback test html&css page + test functionals

--- app/app.py
+++ app/app.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, flash, request
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fKdwKEDPe'
+
+menu = [
+    {'name': 'Dashboard', 'url': 'dashboard'},
+    {'name': 'About', 'url': 'about'},
+    {'name': 'Support', 'url': 'support'},
+    {'name': 'Feedback', 'url': 'feedback'},
+]
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', menu=menu)
 
 
 @app.route('/dashboard')
@@ -38,6 +46,13 @@ def source():
 @app.route('/support')
 def support():
     return redirect('https://github.com/AltDaze/dashboard/issues')
+
+
+@app.route('/feedback', methods=["POST", "GET"])
+def feedback():
+    if request.method == 'POST':
+        #  todo: add checker + else 'error sent'
+        flash("message's sent")
 
 
 if __name__ == "__main__":
