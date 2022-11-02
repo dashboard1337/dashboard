b843a1a1e1d35d2e2c6775e760a1970b81709a6a
AltDaze <dazemoda@gmail.com>
2022-11-02 12:52:51 +0300

    Create index, source, about pages

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -8,6 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 
 @app.route('/login')
@@ -20,9 +25,14 @@ def signup():
     return render_template('signup.html')
 
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/source')
+def source():
+    return redirect('https://github.com/AltDaze/dashboard')
 
 
 if __name__ == "__main__":
